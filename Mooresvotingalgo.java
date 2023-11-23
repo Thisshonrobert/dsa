8c7fcd2e8eb104956a7dd882e12f01c831667c02
thisshonrobertRS <thisshonrobert0205@gmail.com>
2023-11-23 22:15:35 +0530

    greater than n/2 using moore's voting algo

---
+++ Mooresvotingalgo.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Mooresvotingalgo {
+
+    public static void main(String[] args) {
+
+        int arr[] = { 1, 1, 2, 2, 3, 1, 1, };
+        int count = 0, ans = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (count == 0) {
+                count = 1;
+                ans = arr[i];
+            } else if (arr[i] == ans)
+                count++;
+            else
+                count--;
+        }
+        System.out.println(ans);
+    }
+}
